0c240446afe92441f23947085c2d8db94e781106
mostafaaamen <88153348+mostafaaamen@users.noreply.github.com>
2024-10-31 04:14:21 -0400

    Update courses.routes.js

--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -1,11 +1,9 @@
 import { Router } from "express"
-import courseContrallers from "../controllers/courses.controller.js"
-
-import { validationSchema } from "../middleware/validationsCourses.js"
-import { verfiyToken } from "../middleware/verfiyToken.js"
-import { allowTo }from "../middleware/allowTo.js"
-
-import { userRoles } from "../utils/user.rolers.js"
+import courseContrallers from "../Controllers/courses.controller.js"
+import { validationSchema } from "../Middleware/validationsCourses.js"
+import { verfiyToken } from "../Middleware/verfiyToken.js"
+import { allowTo }from "../Middleware/allowTo.js"
+import { userRoles } from "../Utils/user.rolers.js"
 
 
 const router=Router()
